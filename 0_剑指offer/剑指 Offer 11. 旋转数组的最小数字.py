fa94523ebc3d84892ac2a8c1dbc6abbde45ed46c
lijingxin6 <zuibanglijingxin@gmail.com>
2020-09-03 17:14:38 +0800

    add another solution

--- 0_剑指offer/剑指 Offer 11. 旋转数组的最小数字.py
+++ 0_剑指offer/剑指 Offer 11. 旋转数组的最小数字.py
@@ -5,7 +5,7 @@ author: lijingxin
 Created on 下午12:51 2020/7/5
 # 154 #
 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。
-例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。  
+例如，数组[3,4,5,1,2] 为 [1,2,3,4,5] 的一个旋转，该数组的最小值为1。
 
 示例 1：
 输入：[3,4,5,1,2]
@@ -37,3 +37,18 @@ class Solution:
                 right = mid
 
         return nums[mid]
+
+    def minNumberInRotateArray(self, rotateArray):
+        left, right = 0, len(rotateArray) - 1
+        while left < right:
+            mid = (left + right) // 2
+            # 因为low+high在low和high特别大的时候可能会造成溢出，使用减法避免了溢出发生
+            # mid = left + (right - left) // 2
+            if rotateArray[mid] > rotateArray[right]:
+                left = mid + 1
+            elif rotateArray[mid] < rotateArray[right]:
+                right = mid
+            else:
+                right -= 1  # 缩小范围
+        return rotateArray[left]
+
