06195f8c41143c9340ae0e738ac50f0e71fb2db0
lijingxin6 <zuibanglijingxin@gmail.com>
2020-09-10 21:18:50 +0800

    Add Solution 3

--- 0_ByteDance/2-9*.接雨水.py
+++ 0_ByteDance/2-9*.接雨水.py
@@ -62,6 +62,23 @@ class Solution2:
                 ans += max_right - height[right]
                 right -= 1
         return ans
+class Solution3:
+    # https://leetcode-cn.com/problems/trapping-rain-water/solution/shuang-zhi-zhen-an-xing-qiu-geng-hao-li-jie-onsuan/
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        left, right = 0, n-1
+        SUM, tmp, high = 0, 0, 1
+        while(left <= right):
+            while(left <= right and height[left] < high):
+                SUM += height[left]
+                left += 1
+            while(right >= left and height[right] < high):
+                SUM += height[right]
+                right -= 1
+            high += 1
+            tmp += right - left + 1
+        return tmp - SUM
+
 def stringToIntegerList(input):
     return json.loads(input)
 
