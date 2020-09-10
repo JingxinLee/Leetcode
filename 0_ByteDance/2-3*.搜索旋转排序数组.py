8e778ba7ee53f23a6ef3c9497735d32601b0702b
lijingxin6 <zuibanglijingxin@gmail.com>
2020-09-10 20:39:59 +0800

    增加 数组与排序 高频面试题

--- 0_ByteDance/2-3*.搜索旋转排序数组.py
+++ 0_ByteDance/2-3*.搜索旋转排序数组.py
@@ -21,3 +21,57 @@ Created on 5:22 PM 9/9/20
 输入: nums = [4,5,6,7,0,1,2], target = 3
 输出: -1
 """
+import json
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums: return -1
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] >= nums[left]: # 中间比左边大,左边是排序的. 必须有 = 因为两个数的时候left和mid指向的同一个数字
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else: # 中间比左边小,说明右边是排序的
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
+
+
+
+def stringToIntegerList(input):
+    return json.loads(input)
+
+
+def main():
+    import sys
+    import io
+    def readlines():
+        with open('stdin.txt') as f:
+            for line in f:
+                yield line.strip(('\n'))
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            nums = stringToIntegerList(line);
+            line = next(lines)
+            target = int(line);
+
+            ret = Solution().search(nums, target)
+
+            out = str(ret);
+            print(out)
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
--- 0_ByteDance/2-8*.朋友圈.py
+++ 0_ByteDance/2-8*.朋友圈.py
@@ -35,3 +35,120 @@ Created on 5:24 PM 9/9/20
 M[i][i] == 1
 M[i][j] == M[j][i]
 """
+import json
+
+class Solution(object):
+    def findCircleNum(self, M): # DFS 1
+        """
+        :type M: List[List[int]]
+        :rtype: int
+        """
+        N = len(M)
+        count = 0
+        visited = set()
+
+        def dfs(i):
+            for j in range(N):
+                if M[i][j] and j not in visited:
+                    visited.add(j)
+                    dfs(j)
+        for i in range(N):
+            if i not in visited:
+                count += 1
+                visited.add(i)
+                dfs(i)
+        return count
+
+class Solution2(object):
+    def dfs(self, M, visited, i):
+        for j in range(len(M)):
+            if M[i][j] == 1 and visited[j] == 0:
+                visited[j] = 1
+                self.dfs(M, visited, j)
+
+    def findCircleNum(self, M): # DFS 2
+        """
+        :type M: List[List[int]]
+        :rtype: int
+        """
+        visited = [0] * len(M)
+        count = 0
+        for i in range(len(M)):
+            if visited[i] == 0:
+                visited[i] = 1
+                self.dfs(M, visited, i)
+                count += 1
+        return count
+
+class Solution3(object): # BFS 1
+    def findCircleNum(self, M):
+        """
+        :type M: List[List[int]]
+        :rtype: int
+        """
+        N, visited, res = len(M), set(), 0
+        for i in range(N):
+            if i not in visited:
+                queue = [i]
+                while queue:
+                    p = queue.pop(0)
+                    if p not in visited:
+                        visited.add(p)
+                        #queue += [k for k, num in enumerate(M[p]) if num and k not in visited]
+                        for k,num in enumerate(M[p]):
+                            if num and k not in visited:
+                                queue += [k]
+                res += 1
+        return res
+class Solution4: # BFS 2
+    def findCircleNum(self, M: List[List[int]]) -> int:
+        n = len(M)  ##节点数量
+        vis = set() ##访问过的节点记录
+        ans = 0    ##连通分量计数
+        for i in range(n):    ##遍历所有节点，作为搜索起点
+            if i not in vis:  ##只处理没有访问过的节点
+                queue = deque([i])    ##将首节点加入队列
+                vis.add(i)    ##每加入一次队列，记录一次访问
+
+                while queue:##bfs过程
+                    q = queue.popleft()
+                    for j in range(n):
+                        if M[q][j] == 1 and j not in vis:  ##加入队列之前先检查一次是否访问过
+                            queue.append(j)
+                            vis.add(j)
+                ans += 1
+        return ans
+
+def stringToInt2dArray(input):
+    return json.loads(input)
+
+
+def intToString(input):
+    if input is None:
+        input = 0
+    return str(input)
+
+
+def main():
+    import sys
+    def readlines():
+        with open('stdin.txt') as f:
+            for line in f:
+                yield line.strip(('\n'))
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            M = stringToInt2dArray(line)
+
+            ret = Solution3().findCircleNum(M)
+
+            out = intToString(ret)
+            print(out)
+        except StopIteration:
+            break
+
+
+if __name__ == '__main__':
+    main()
