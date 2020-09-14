e6f7e34044069dec0d44d05fbd3842608c3bc881
lijingxin6 <zuibanglijingxin@gmail.com>
2020-09-14 22:02:36 +0800

    Add Solution

--- 0_ByteDance/4-4*.最大子序和.py
+++ 0_ByteDance/4-4*.最大子序和.py
@@ -18,7 +18,19 @@ import json
 from typing import List
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-
+        for i in range(1, len(nums)):
+            nums[i] += max(nums[i - 1], 0)
+        return max(nums)
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxans, cur = nums[0], 0
+        for num in nums:
+            if cur < 0:
+                cur = num
+            else:
+                cur += num
+            if maxans < cur:
+                maxans = cur
+        return maxans
 
 def stringToIntegerList(input):
     return json.loads(input)
